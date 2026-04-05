adb751a2202ee7f1b9ed323530e5995ede9831c5
Dan Ruan <danrsaquetti@gmail.com>
2026-04-05 15:48:46 -0300

    feat: Ajustando componente de card de projetos para exibir novas propriedades

--- src/domain/interfaces/Projeto.ts
+++ src/domain/interfaces/Projeto.ts
@@ -3,8 +3,14 @@ export type ProjetoLevel = "iniciante" | "intermediário" | "avançado"
 export interface Projeto {
   slug: string
   titulo: string
+  resumo: string
   descricao: string
   level: ProjetoLevel
+  categoria: string
+  tags: string[]
+  idadeMinima: number
+  duracao: number
+  autor: string
   imagem: {
     src: string
     alt?: string
