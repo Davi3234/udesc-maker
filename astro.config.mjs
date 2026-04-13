0df0107df179359a65355e897e140b4242fa7930
Dan Ruan <danrsaquetti@gmail.com>
2026-04-13 20:15:00 -0300

    config: Removendo configuração de `output: server`

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,5 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://davi3234.github.io',
-  base: '/udesc-maker',
-  output: 'server'
+  base: '/udesc-maker'
 });
