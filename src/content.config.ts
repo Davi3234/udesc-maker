c84ede5842e09606e7cb2652ce13335fe42f6eff
Dan Ruan <danrsaquetti@gmail.com>
2026-04-12 21:18:46 -0300

    feat: Criando collection de projetos

---
+++ src/content.config.ts
@@ -0,0 +1,22 @@
+import { glob } from 'astro/loaders';
+import { z } from 'astro/zod';
+import { defineCollection } from 'astro:content';
+import { ProjetoLevel } from './domain/interfaces/Projeto';
+
+const projetos = defineCollection({
+  loader: glob({ pattern: '**/content.md', base: './src/content/projects' }),
+  schema: ({ image }) => z.object({
+    titulo: z.string(),
+    autor: z.string(),
+    descricao: z.string().optional(),
+    resumo: z.string().optional(),
+    level: z.enum(ProjetoLevel).default(ProjetoLevel.Iniciante),
+    categoria: z.string().default('Geral'),
+    tags: z.array(z.string()).default([]),
+    idadeMinima: z.int().nonnegative().optional(),
+    duracao: z.number().nonnegative().optional(),
+    imagem: image().optional(),
+  }),
+});
+
+export const collections = { projetos };
