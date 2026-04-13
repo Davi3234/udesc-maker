52c5281e8b7e149eeea10f39811aba9183d1a33d
Dan Ruan <danrsaquetti@gmail.com>
2026-04-12 22:27:43 -0300

    feat: Criando método para busca dos dados e conteúdo do projeto `ProjetoService.get`

--- src/services/projeto.service.ts
+++ src/services/projeto.service.ts
@@ -1,21 +1,48 @@
-import { getCollection } from "astro:content";
+import { getCollection, type RenderedContent } from "astro:content";
 import type { Projeto } from "../domain/interfaces/Projeto";
+
+export interface ProjetoContent {
+  data: Projeto
+  body?: RenderedContent
+}
 
 export class ProjetoService {
 
   async listAll(): Promise<Projeto[]> {
     const projetos = await getCollection('projetos')
 
-    return projetos.map(({ data, filePath }) => {
-      const parts = `${filePath}`.split("/")
+    const pro = projetos.map(({ data, filePath }) => ({
+      ...data,
+      slug: this.makeSlugFromPath(filePath)
+    }))
 
-      parts.pop()
-      const slug = parts.pop();
+    return pro
+  }
 
-      return {
-        ...data,
-        slug: slug ?? ''
+  async get(slug: string): Promise<ProjetoContent> {
+    const projetos = await getCollection('projetos')
+
+    for (let i = 0; i < projetos.length; i++) {
+      const projectSlug = this.makeSlugFromPath(projetos[i].filePath)
+
+      if (projectSlug == slug) {
+        return {
+          data: {
+            ...projetos[i].data,
+            slug: projectSlug,
+          },
+          body: projetos[i].rendered
+        }
       }
-    })
+    }
+
+    throw new Error(`Projeto ${slug} não encontrado`)
+  }
+
+  private makeSlugFromPath(path?: string) {
+    const parts = `${path}`.split("/")
+
+    parts.pop()
+    return parts.pop() ?? ''
   }
 }
