24c76fdad8185f075f73086b8f2cb3dfd2e7bd02
Dan Ruan <danrsaquetti@gmail.com>
2026-04-12 21:20:02 -0300

    feat: Atualizando propriedades interface `Projeto`

---
+++ src/@types/image.ts
@@ -0,0 +1,6 @@
+export type ImageData = {
+  src: string
+  width?: number
+  height?: number
+  format?: "svg" | "png" | "jpg" | "jpeg" | "tiff" | "webp" | "gif" | "avif"
+}
--- src/domain/interfaces/Projeto.ts
+++ src/domain/interfaces/Projeto.ts
@@ -1,19 +1,21 @@
-export type ProjetoLevel = "iniciante" | "intermediário" | "avançado"
+import type { ImageData } from "../../@types/image"
+
+export enum ProjetoLevel {
+  Iniciante = 'iniciante',
+  Intermediario = 'intermediário',
+  Avancado = 'avançado',
+}
 
 export interface Projeto {
   slug: string
   titulo: string
-  resumo: string
-  descricao: string
+  resumo?: string
+  descricao?: string
   level: ProjetoLevel
   categoria: string
   tags: string[]
-  idadeMinima: number
-  duracao: number
+  idadeMinima?: number
+  duracao?: number
   autor: string
-  imagem: {
-    src: string
-    alt?: string
-  }
-  icon?: "robot" | "wood" | "circuit" | "default"
+  imagem?: ImageData
 }
